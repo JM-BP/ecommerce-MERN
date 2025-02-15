f9e2773a85416119146624bbf529ffe454527fb6
Juan Manuel Balastegui Perez <bp.juanma96@gmail.com>
2025-02-15 13:32:33 +0100

    Agregar la barra de navegación principal

---
+++ src/assets/assets.js
@@ -0,0 +1,29 @@
+import web_logo from './logo.webp'
+import esdla_lcda from './esdla-lcda.jpg'
+import search_icon from './search-icon.png'
+import profile_icon from './profile-icon.png'
+import cart_icon from './cart-icon.png'
+import menu_icon from './menu-icon.png'
+import back_icon from './back-icon.png'
+
+export const assets = {
+    web_logo,
+    esdla_lcda,
+    search_icon,
+    profile_icon,
+    cart_icon,
+    menu_icon,
+    back_icon
+}
+
+export const books = [
+    {
+        _id: "aaaaaa",
+        name: "El señor de los anillos: La comunidad del anillo",
+        author: "J. R. R. Tolkien",
+        image: [esdla_lcda],
+        editorial: "MINOTAURO",
+        description: "Empieza tu viaje a la Tierra Media. Los libros que han inspirado la nueva serie EL SEÑOR DE LOS ANILLOS: LOS ANILLOS DEL PODER. La primera entrega de la trilogía de J. R. R. Tolkien El Señor de los Anillos. En la adormecida e idílica Comarca, un joven hobbit recibe un encargo: custodiar el Anillo Único y emprender el viaje para su destrucción en la Grieta del Destino. Acompañado por magos, hombres, elfos y enanos, atravesará la Tierra Media y se internará en las sombras de Mordor, perseguido siempre por las huestes de Sauron, el Señor Oscuro, dispuesto a recuperar su creación para establecer el dominio definitivo del Mal. «La obra de Tolkien, difundida en millones de ejemplares, traducida a docenas de lenguas, inspiradora de slogans pintados en las paredes de Nueva York y de Buenos Aires... una coherente mitología de una autenticidad universal creada en pleno siglo veinte.» âeuros'George Steiner, Le Monde, 1973",
+        price: "15"
+    }
+]
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react'
+import { assets } from '../assets/assets'
+import { Link, NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <div className='flex items-center justify-between py-5 font-bold'>
+      <img src={assets.web_logo} className='w-24 rounded-full' alt="web-logo" />
+      
+      <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
+        <NavLink to='/' className='flex flex-col items-center gap-1'>
+          <p>INICIO</p>
+          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
+        </NavLink>
+        <NavLink to='/collection' className='flex flex-col items-center gap-1'>
+          <p>COLECCIÓN</p>
+          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
+        </NavLink>
+        <NavLink to='/about' className='flex flex-col items-center gap-1'>
+          <p>ACERCA DE</p>
+          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
+        </NavLink>
+        <NavLink to='/contact' className='flex flex-col items-center gap-1'>
+          <p>CONTACTO</p>
+          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
+        </NavLink>
+      </ul>
+      <div className='flex items-center gap-6'>
+        <img src={assets.search_icon} className='w-5 cursor-pointer' alt="search-icon" />
+          <div className='group relative'>
+            <img src={assets.profile_icon} className='w-5 cursor-pointer' alt="profile-icon" />
+            <div className='group-hover:block hidden absolute dropdow-menu right-0 pt-4'>
+              <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+                <p className='cursor-pointer hover:text-black'>Mi perfil</p>
+                <p className='cursor-pointer hover:text-black'>Mis pedidos</p>
+                <p className='cursor-pointer hover:text-black'>Cerrar sesión</p>
+              </div>
+            </div>
+          </div>
+          <Link to="/cart" className="relative">
+            <div className="relative">
+              <img src={assets.cart_icon} className="w-5 min-w-5" alt="cart-icon" />
+              <p className="absolute top-5 right-0 translate-x-1/2 translate-y-[-50%] w-4 h-4 flex items-center justify-center bg-black text-white rounded-full text-[8px]">
+                10
+              </p>
+            </div>
+          </Link>
+          <img onClick={()=>setVisible(true)} src={assets.menu_icon} className='w-8 cursor-pointer sm:hidden' alt='menu-icon' />
+      </div>
+        {/* Sidebar menu for small screens*/}
+        <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`}>
+          <div className='flex flex-col text-gray-600'>
+            <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
+              <img className='size-5' src={assets.back_icon} alt="back-icon" />
+              <p>Back</p>
+            </div>
+            <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/'>INICIO</NavLink>
+            <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/collection'>COLECCIÓN</NavLink>
+            <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/about'>ACERCA DE</NavLink>
+            <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/contact'>CONTACTO</NavLink>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
