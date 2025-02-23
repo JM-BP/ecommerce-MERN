230406874a87622ea6e2a39f3276dbca6c3cd144
Juan Manuel Balastegui Perez <bp.juanma96@gmail.com>
2025-02-23 16:10:53 +0100

    Añadir la lógica de registro de usuarios con validación y generación de token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,64 @@
 import { response } from "express";
+import userModel from "../models/userModel.js";
+import validador from "validator";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET);
+};
 
 // Route for user login
 const loginUser = async (req, res) => {};
 
 // Route for user register
 const registerUser = async (req, res) => {
-  res.json({ msg: "Register API Working" });
+  try {
+    const { name, email, password } = req.body;
+
+    //Checking user
+    const exists = await userModel.findOne({ email });
+    if (exists) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+
+    //Validating email and password
+    if (!validador.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "Please enter a valid email",
+      });
+    }
+
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "Pasword must have 8 characters or more",
+      });
+    }
+
+    //Hashing user password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const user = await newUser.save();
+
+    const token = createToken(user._id);
+
+    res.json({
+      success: true,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
 };
 
 // Route for admin login
