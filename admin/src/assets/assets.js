3e29f39e179157118544794e7d5f679de10bf7bf
Juan Manuel Balastegui Perez <bp.juanma96@gmail.com>
2025-03-03 14:10:01 +0100

    Añadir la lógica de login para administrador

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,6 @@
+import logo from "./logo.png";
+import add from "./add-icon.png";
+import list from "./list-icon.png";
+import order from "./order-icon.png";
+
+export const assets = { logo, add, list, order };
